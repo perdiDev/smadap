eaddce909b30f2ab750a605a675cc76a991ed408
PerdiDev <perdidev@gmail.com>
2023-02-12 20:02:47 +0800

    Halaman pengumuman

---
+++ resources/views/pengumuman.blade.php
@@ -0,0 +1,124 @@
+
+<section class="container bg-white">
+    <div class="mx-auto mb-4 max-w-screen-xl py-8">
+        <div class="w-full space-y-2 py-4 text-center md:mb-8">
+            <h2
+            class="mb-4 text-3xl font-extrabold tracking-tight text-gray-900 lg:text-4xl"
+            >
+            Pengumuman
+            </h2>
+            <div class="flex w-full flex-wrap justify-center">
+            <div
+                class="w-fit space-x-4 space-y-2 rounded-md bg-yellow-100 py-2 px-4 md:space-y-0"
+            >
+                <button class="rounded bg-white py-1 px-2 text-yellow-300">
+                Semua
+                </button>
+                <button class="hover:text-yellow-300">Umum</button>
+                <button class="hover:text-yellow-300">Kelas X</button>
+                <button class="hover:text-yellow-300">Kelas XI</button>
+                <button class="hover:text-yellow-300">Kelas XII</button>
+            </div>
+            </div>
+        </div>
+        <div class="grid gap-8 lg:grid-cols-2">
+            <article
+            class="rounded-lg border border-gray-200 bg-white p-6 shadow-md"
+            >
+            <div class="mb-5 flex items-center justify-between text-gray-500">
+                <span
+                class="bg-primary-100 text-primary-800 inline-flex items-center rounded bg-yellow-100 px-2.5 py-0.5 text-xs font-medium"
+                >
+                Umum
+                </span>
+                <span class="text-sm">14 days ago</span>
+            </div>
+            <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                <a href="#">Jadwal ujian akhir semester genap</a>
+            </h2>
+            <p class="mb-5 font-light text-gray-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque
+                provident, fuga id fugit voluptate labore tenetur maiores
+                consequuntur nemo nam. Sint animi, suscipit repellat ullam
+                distinctio quis ducimus sit illum.
+            </p>
+            <div class="flex items-center justify-between">
+                <div class="flex items-center space-x-4">
+                <img
+                    class="h-7 w-7 rounded-full"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+                    alt="Jese Leos avatar"
+                />
+                <span class="font-medium dark:text-white"> Perdi S.Pd. </span>
+                </div>
+                <a
+                href="#"
+                class="group inline-flex items-center font-medium text-yellow-300 hover:underline"
+                >
+                Lihat Detail
+                <svg
+                    class="ml-2 h-4 w-4 group-hover:translate-x-1"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                    ></path>
+                </svg>
+                </a>
+            </div>
+            </article>
+            <article
+            class="rounded-lg border border-gray-200 bg-white p-6 shadow-md"
+            >
+            <div class="mb-5 flex items-center justify-between text-gray-500">
+                <span
+                class="bg-primary-100 text-primary-800 inline-flex items-center rounded bg-yellow-100 px-2.5 py-0.5 text-xs font-medium"
+                >
+                Kelas X
+                </span>
+                <span class="text-sm">14 days ago</span>
+            </div>
+            <h2 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                <a href="#">Jadwal ujian akhir semester genap</a>
+            </h2>
+            <p class="mb-5 font-light text-gray-500">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
+                corporis ipsum ratione sapiente rerum aliquid est, illum ipsam
+                voluptatum adipisci? Quas cupiditate natus sunt rem.
+            </p>
+            <div class="flex items-center justify-between">
+                <div class="flex items-center space-x-4">
+                <img
+                    class="h-7 w-7 rounded-full"
+                    src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
+                    alt="Jese Leos avatar"
+                />
+                <span class="font-medium dark:text-white"> Perdi S.Pd. </span>
+                </div>
+                <a
+                href="#"
+                class="group inline-flex items-center font-medium text-yellow-300 hover:underline"
+                >
+                Lihat Detail
+                <svg
+                    class="ml-2 h-4 w-4 group-hover:translate-x-1"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                >
+                    <path
+                    fill-rule="evenodd"
+                    d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                    clip-rule="evenodd"
+                    ></path>
+                </svg>
+                </a>
+            </div>
+            </article>
+        </div>
+    </div>
+</section>
